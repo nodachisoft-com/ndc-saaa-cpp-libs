3f190a47bfb8617e03997ac03e27c3b0975b199c
yukutonet <amaji.yukuto@gmail.com>
2023-04-03 00:53:44 +0900

    不要なコメント削除

--- ndc-cpp-libs/src/image/BitmapImage.cpp
+++ ndc-cpp-libs/src/image/BitmapImage.cpp
@@ -26,11 +26,6 @@ BitmapImage::~BitmapImage()
 {
   // 画像本体データを解放する
   delete imgp;
-  //if (fontImage != nullptr)
-  //{
-    // フォントデータ読み込み済みなら解放する
-  //  delete fontImage;
-  //}
 }
 
 void BitmapImage::ReadBmp(const std::string filename)
@@ -233,88 +228,3 @@ void BitmapImage::PrintBmpInfo(const char *filename)
 
   fclose(Bmp_Fp);
 }
-
-//void BitmapImage::set(int x, int y, ColorRGB &color)
-//{
-//  imgp->set(x,y,color);
-//}
-
-/*
-ColorRGB BitmapImage::get(int x, int y)
-{
-  return imgp->get(x,y);
-}
-
-long BitmapImage::getWidth()
-{
-  return metainfo.Bmp_width;
-}
-
-long BitmapImage::getHeight()
-{
-  return metainfo.Bmp_height;
-}
-
-void BitmapImage::clear(ColorRGB &color)
-{
-  imgp->clear(color);
-}
-
-
-void BitmapImage::writeText(const int destBeginX, const int destBeginY, const std::string text, ColorRGB &color)
-{
- 
-  size_t size = text.size();
-  for (size_t index = 0; index < size; index++)
-  {
-    const char drawCh = text[index];
-    writeChar(destBeginX + index * DebugFontData::width, destBeginY, drawCh, color);
-  }
-  
-  imgp->writeText(destBeginX, destBeginY, text, color);
-}
-*/
-
-//void BitmapImage::writeChar(const int destBeginX, const int destBeginY, const char ch, ColorRGB &color)
-//{
-//  imgp->writeChar(destBeginX, destBeginY, ch, color);
-  // 描画対象の範囲外の文字コードが渡された
-  /*
-  if (ch < ' ' || '~' < ch)
-  {
-    // 何も描画しない
-    return;
-  }
-  // コピーするフォント画像データの開始 x 座標を求める
-  int fontImgBeginX = (ch - ' ') * DebugFontData::width;
-
-  for (int y = 0; y < DebugFontData::height; y++)
-  {
-    for (int x = 0; x < DebugFontData::width; x++)
-    {
-      int destX = destBeginX + x;
-      int destY = destBeginY + y;
-      if ((destX < 0) || (destX >= metainfo.Bmp_width) ||
-          (destY < 0) || (destY >= metainfo.Bmp_height))
-      {
-        // 描画先がデータ範囲外
-        continue;
-      }
-
-      // フォント画像を書き込み。白色は透明化
-      // フォント画像が黒色に近い程、フォント色側に書き込み先の元色を近づけることで、印字を表現。
-      ColorRGB fontBaseColor = DEBUG_FONT_DATA.getFontCanvas()->get(fontImgBeginX + x, y);
-      ColorRGB beforeColor = get(destX, destY);
-
-      // 0.0f は字に近い。1.0f は字から遠い
-      float distance = 1.0f - (fontBaseColor.r / 255.0f);
-      ColorRGB updateColor;
-      updateColor.r = (unsigned char)(color.r * distance + beforeColor.r * (1.0f - distance));
-      updateColor.g = (unsigned char)(color.g * distance + beforeColor.g * (1.0f - distance));
-      updateColor.b = (unsigned char)(color.b * distance + beforeColor.b * (1.0f - distance));
-
-      set(destX, destY, updateColor);
-    }
-  }
-  */
-//}
--- ndc-cpp-libs/src/image/BitmapImage.hpp
+++ ndc-cpp-libs/src/image/BitmapImage.hpp
@@ -47,47 +47,6 @@ namespace nl
     void PrintBmpInfo(const std::string filename);
     void PrintBmpInfo(const char *filename);
 
-    /// @brief 色情報をバッファに書き込む
-    /// @param[in] x 書き込む画像内の位置（左を原点とする）
-    /// @param[in] y 書き込む画像内の位置（上を原点とする）
-    /// @param[in] color 書き込む色情報（RGB）
-    //void set(int x, int y, ColorRGB &color);
-
-    /// @brief 指定の画像位置から色情報を取得する
-    /// @param[in] x 書き込む画像内の位置（左を原点とする）
-    /// @param[in] y 書き込む画像内の位置（上を原点とする）
-    /// @return 取得した色情報（RGB）
-    //ColorRGB get(int x, int y);
-
-    /// @brief 画像の横方向ピクセル数を取得する
-    //long getWidth();
-
-    /// @brief 画像の縦方向ピクセル数を取得する
-    //long getHeight();
-
-    /// @brief 画像を指定の色で塗りつぶす
-    /// @param[in] color 塗りつぶす色
-    //void clear(ColorRGB &color);
-
-    /// @brief
-    ///  等幅フォント（1文字当たり11px、高さ 21px）で文字を描画する。
-    ///  利用する初回にフォント画像情報を読み込む。
-    /// @param[in] x 文字を描画する開始位置 x 座標
-    /// @param[in] y 文字を描画する開始位置 y 座標
-    /// @param[in] text 描画する文字列
-    /// @param[in] color フォントの色情報（RGB）
-    //void writeText(const int x, const int y, const std::string text, ColorRGB &color);
-
-    /// @brief
-    ///  等幅フォント（1文字当たり11px、高さ 21px）で１文字を描画する。
-    ///  利用する初回にフォント画像情報を読み込む。
-    /// @param[in] x 文字を描画する開始位置 x 座標
-    /// @param[in] y 文字を描画する開始位置 y 座標
-    /// @param[in] oneText 描画する文字
-    /// @param[in] color フォントの色情報（RGB）
-    //void writeChar(const int x, const int y, const char oneText, ColorRGB &color);
-
-
     /// <summary>
     /// BitmapImage に保持されている ImageCanvas への参照を取得する
     /// </summary>
